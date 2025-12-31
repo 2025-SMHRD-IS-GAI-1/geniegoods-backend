83158126d20cb8d04d3fb314d1c59a4cd401f065
pobiq <hoon2207@naver.com>
2025-12-31 09:35:50 +0900

    final 추가

--- src/main/java/com/example/geniegoods/controller/UserRestController.java
+++ src/main/java/com/example/geniegoods/controller/UserRestController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class UserRestController {
 
-    private UserService userService;
+    private final UserService userService;
 
     /**
      * 토큰 발급 (포스트맨 테스트용)
